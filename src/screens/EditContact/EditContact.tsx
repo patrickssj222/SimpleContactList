c114a4dbb58db7277cc4b9c8942fe2557e15bedb
Yun Hao <patrickssj222@gmail.com>
2021-03-21 18:01:36 -0400

    added keyboard dismiss to edit page

--- src/screens/EditContact/EditContact.tsx
+++ src/screens/EditContact/EditContact.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from "react";
-import {Text, TextInput, View} from "react-native";
+import {Keyboard, Text, TextInput, TouchableWithoutFeedback, View} from "react-native";
 import {styles} from "./EditContactStyle";
 import {CustomButton, Header} from "../../component";
 import {useDispatch, useSelector} from "react-redux";
@@ -41,37 +41,39 @@ export const EditContact = (props: Props) => {
     const removeContact = () => {
         dispatch({type:SAGA_REMOVE_CONTACT, payload:{id:contact.id, setLoading: setRemoveLoading, callBack}})
     }
-    return <View style={styles.container}>
-        <Header title={contact.firstName+" "+contact.lastName} color={GlobalStyle.color.primaryColor} isFocused={isFocused}/>
-        <View style={styles.inputWrapper}>
-            <View style={styles.inputItemWrapper}>
-                <Text>First Name</Text>
-                <TextInput style={styles.inputItem} value={firstName} placeholder={"First Name"} onChangeText={setFirstName}/>
+    return <TouchableWithoutFeedback onPress={()=>Keyboard.dismiss()}>
+        <View style={styles.container}>
+            <Header title={contact.firstName+" "+contact.lastName} color={GlobalStyle.color.primaryColor} isFocused={isFocused}/>
+            <View style={styles.inputWrapper}>
+                <View style={styles.inputItemWrapper}>
+                    <Text>First Name</Text>
+                    <TextInput style={styles.inputItem} value={firstName} placeholder={"First Name"} onChangeText={setFirstName}/>
+                </View>
+                <View style={styles.inputItemWrapper}>
+                    <Text>Last Name</Text>
+                    <TextInput style={styles.inputItem} value={lastName} placeholder={"Last Name"} onChangeText={setLastName}/>
+                </View>
+                <View style={styles.inputItemWrapper}>
+                    <Text>Company Name</Text>
+                    <TextInput style={styles.inputItem} value={companyName} placeholder={"Company Name"} onChangeText={setCompanyName}/>
+                </View>
             </View>
-            <View style={styles.inputItemWrapper}>
-                <Text>Last Name</Text>
-                <TextInput style={styles.inputItem} value={lastName} placeholder={"Last Name"} onChangeText={setLastName}/>
-            </View>
-            <View style={styles.inputItemWrapper}>
-                <Text>Company Name</Text>
-                <TextInput style={styles.inputItem} value={companyName} placeholder={"Company Name"} onChangeText={setCompanyName}/>
+            <View style={styles.footer}>
+                <CustomButton
+                    text={"Submit"}
+                    buttonStyle={{marginVertical: 5, backgroundColor: GlobalStyle.color.tertiaryColor, borderWidth: 0}}
+                    textStyle={{color: GlobalStyle.color.primaryColor}}
+                    onPress={editContact}
+                    showActivityIndicator={editLoading}
+                />
+                <CustomButton
+                    text={"Delete Contact"}
+                    buttonStyle={{marginVertical: 5, backgroundColor: GlobalStyle.color.negativeColor, borderWidth: 0}}
+                    textStyle={{color: GlobalStyle.color.primaryColor}}
+                    onPress={removeContact}
+                    showActivityIndicator={removeLoading}
+                />
             </View>
         </View>
-        <View style={styles.footer}>
-            <CustomButton
-                text={"Submit"}
-                buttonStyle={{marginVertical: 5, backgroundColor: GlobalStyle.color.tertiaryColor, borderWidth: 0}}
-                textStyle={{color: GlobalStyle.color.primaryColor}}
-                onPress={editContact}
-                showActivityIndicator={editLoading}
-            />
-            <CustomButton
-                text={"Delete Contact"}
-                buttonStyle={{marginVertical: 5, backgroundColor: GlobalStyle.color.negativeColor, borderWidth: 0}}
-                textStyle={{color: GlobalStyle.color.primaryColor}}
-                onPress={removeContact}
-                showActivityIndicator={removeLoading}
-            />
-        </View>
-    </View>
+    </TouchableWithoutFeedback>
 }
